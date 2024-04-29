f8ef61d80cd813690a0dbd0f4d2363f01fe1078f
murattok <murat_tok@me.com>
2024-04-29 15:01:53 +0600

    calculator programm done

--- main.py
+++ main.py
@@ -20,22 +20,42 @@ def input_operation():
 
     return operation
 
-def input_number():
+def input_number(step):
     while True:
         number = input('Введите {step} чило: ')
 
         if not number.isdigit():
-            print('Введите числоб повторите!!!')
+            print('Введите число повторите!!!')
             continue
 
-        return number
+        return float(number)
+
+def solve(number_one, number_two, opertion):
+    result = None
+    if opertion == '+':
+        result = number_one + number_two
+    elif opertion == '-':
+        result = number_one - number_two
+    elif opertion == '*':
+        result = number_one * number_two
+    else:
+        result = number_one/number_two
+
+    return result
+
+def print_solver(number_one, number_two, operation, result):
+    print(f'{number_one}{operation}{number_two} = {result}')
+
+
 
 def main():
     calc_info()
     number_one = input_number('первое')
     number_two = input_number('второе')
     operation = input_operation()
-    print(f'{number_one}{operation}{number_two} = результат')
+    result = solve(number_one, number_two, operation)
+    print_solver(number_one, number_two, operation, result)
+    
 
 
 if __name__ == '__main__':
